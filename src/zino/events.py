f4101be15a8394b11814856df483d98ae2455ddd
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-05-10 14:45:38 +0200

    Make Events registry into a Pydantic model
    The registry it self needs to be serializable.  Attributes that are to
    be serialized should be public, while purely internal attributes should
    be marked as private.
    
    The actual list of events and the highest running event id needs to be
    serialized, but the internal lookup index for events by key should not
    be serialized (or it will basically just be a copy of the events list,
    which makes it 2x the size).

--- src/zino/events.py
+++ src/zino/events.py
@@ -2,6 +2,8 @@ import logging
 from collections import namedtuple
 from typing import Optional, Tuple
 
+from pydantic.main import BaseModel
+
 from zino.statemodels import Event, EventState, EventType, PortOrIPAddress
 from zino.time import now
 
@@ -10,17 +12,19 @@ EventIndex = namedtuple("EventIndex", "router port event_type")
 _log = logging.getLogger(__name__)
 
 
-class Events:
-    def __init__(self):
-        self._events = {}
-        self._events_by_index = {}
-        self._last_event_id = 0
+class Events(BaseModel):
+    events: dict = {}
+    last_event_id: int = 0
+    _events_by_index: dict = {}
+
+    class Config:
+        underscore_attrs_are_private = True
 
     def __getitem__(self, item):
-        return self._events[item]
+        return self.events[item]
 
     def __len__(self):
-        return len(self._events)
+        return len(self.events)
 
     def get_or_create_event(
         self, device_name: str, port: Optional[PortOrIPAddress], event_type: EventType
@@ -46,8 +50,8 @@ class Events:
         if index in self._events_by_index:
             raise EventExistsError(f"Event for {index} already exists")
 
-        event_id = self._last_event_id + 1
-        self._last_event_id = event_id
+        event_id = self.last_event_id + 1
+        self.last_event_id = event_id
 
         timestamp = now()
         event = Event(
@@ -59,7 +63,7 @@ class Events:
             opened=timestamp,
             updated=timestamp,
         )
-        self._events[event_id] = event
+        self.events[event_id] = event
         self._events_by_index[index] = event
         _log.debug("created %r", event)
         return event
