8c910bd5481d72ab182cb948d82564ed05cf51b2
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-06-15 10:52:44 +0200

    Improve test coverage for binaries/scripts

--- tests/zino_test.py
+++ tests/zino_test.py
@@ -1,5 +1,20 @@
+import subprocess
+from argparse import Namespace
+
+from zino import zino
+
+
 def test_zino_version_should_be_available():
     from zino import version
 
     assert version.__version__
     assert version.__version_tuple__
+
+
+def test_zino_help_screen_should_not_crash():
+    assert subprocess.check_call(["zino", "--help"]) == 0
+
+
+def test_zino_argparser_works(polldevs_conf):
+    parser = zino.parse_args(["--polldevs", str(polldevs_conf)])
+    assert isinstance(parser, Namespace)
