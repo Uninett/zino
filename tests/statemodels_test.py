e3b18ad83f269f276f8964422e09aeac1ba41326
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-06-21 15:46:56 +0200

    Test DeviceState class

--- tests/statemodels_test.py
+++ tests/statemodels_test.py
@@ -2,7 +2,7 @@ import datetime
 
 import pytest
 
-from zino.statemodels import EventState, ReachabilityEvent
+from zino.statemodels import DeviceState, EventState, ReachabilityEvent
 
 
 class TestEvent:
@@ -13,6 +13,16 @@ class TestEvent:
     def test_add_history_should_set_proper_timestamp(self, fake_event):
         log = fake_event.add_history("test")
         assert isinstance(log.timestamp, datetime.datetime)
+
+
+class TestDeviceState:
+    @pytest.mark.parametrize(
+        "enterprise_id,property_name,expected",
+        [(9, "is_cisco", True), (666, "is_cisco", False), (2626, "is_juniper", True), (666, "is_juniper", False)],
+    )
+    def test_vendor_utility_property_returns_expected_result(self, enterprise_id, property_name, expected):
+        dev = DeviceState(name="foo", enterprise_id=enterprise_id)
+        assert getattr(dev, property_name) == expected
 
 
 @pytest.fixture
