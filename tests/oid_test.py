64413417485caa8c49599ad3c9da455cae9358e7
Simon Oliver Tveit <simtve@uninett.no>
2023-07-03 15:03:18 +0200

    Add strip prefix tests

--- tests/oid_test.py
+++ tests/oid_test.py
@@ -13,3 +13,17 @@ class TestOIDPrefix:
     def test_return_false_if_prefix_equal_to_oid(self):
         oid = OID(".5.4.3.2.1")
         assert not oid.is_a_prefix_of(".5.4.3.2.1")
+
+
+class TestOIDStripPrefix:
+    def test_valid_prefix_is_stripped(self):
+        oid = ".1.2.3.4.5"
+        prefix = ".1.2"
+        stripped_oid = OID(oid).strip_prefix(prefix)
+        assert str(stripped_oid) == ".3.4.5"
+
+    def test_invalid_prefix_is_not_stripped(self):
+        oid = ".1.2.3.4.5"
+        prefix = ".5.4"
+        stripped_oid = OID(oid).strip_prefix(prefix)
+        assert str(stripped_oid) == oid
