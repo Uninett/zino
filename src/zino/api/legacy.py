cf6471d98688163faaf5f5c8ac4f888cb37ff7f5
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-10-18 14:29:35 +0200

    Implement GETLOG API command

--- src/zino/api/legacy.py
+++ src/zino/api/legacy.py
@@ -225,6 +225,21 @@ class Zino1ServerProtocol(Zino1BaseServerProtocol):
 
         self._respond_raw(".")
 
+    @requires_authentication
+    async def do_getlog(self, case_id: Union[str, int]):
+        try:
+            case_id = int(case_id)
+            event = self._state.events[case_id]
+        except (ValueError, KeyError):
+            return self._respond_error(f'event "{case_id}" does not exist')
+
+        self._respond(300, "log follows, terminated with '.'")
+        for log in event.log:
+            for line in log.model_dump_legacy():
+                self._respond_raw(line)
+
+        self._respond_raw(".")
+
 
 class ZinoTestProtocol(Zino1ServerProtocol):
     """Extended Zino 1 server protocol with test commands added in"""
