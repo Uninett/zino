3e3b758485e7145305e4fb87d4ca01e71270c49f
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-10-18 12:02:23 +0200

    Test Event.model_dump_simple_attrs

--- tests/statemodels_test.py
+++ tests/statemodels_test.py
@@ -2,7 +2,13 @@ import datetime
 
 import pytest
 
-from zino.statemodels import DeviceState, DeviceStates, EventState, ReachabilityEvent
+from zino.statemodels import (
+    DeviceState,
+    DeviceStates,
+    Event,
+    EventState,
+    ReachabilityEvent,
+)
 
 
 class TestEvent:
@@ -13,6 +19,34 @@ class TestEvent:
     def test_add_history_should_set_proper_timestamp(self, fake_event):
         log = fake_event.add_history("test")
         assert isinstance(log.timestamp, datetime.datetime)
+
+    def test_model_dump_simple_attrs_should_return_dict_of_only_string_values(self, fake_event):
+        attrs = fake_event.model_dump_simple_attrs()
+
+        assert all(isinstance(val, str) for val in attrs.values())
+
+    def test_model_dump_simple_attrs_should_not_return_dict_with_log_or_history(self, fake_event):
+        attrs = fake_event.model_dump_simple_attrs()
+
+        assert "log" not in attrs
+        assert "history" not in attrs
+
+    def test_zinoify_value_when_value_is_enum_it_should_return_its_real_value(self):
+        assert Event.zinoify_value(EventState.OPEN) == "open"
+
+    def test_zinoify_value_when_value_is_int_it_should_return_a_str(self):
+        assert Event.zinoify_value(42) == "42"
+
+    def test_zinoify_value_when_value_is_datetime_it_should_return_a_unix_timestamp_as_a_str(self):
+        timestamp = datetime.datetime(2023, 10, 18, 11, 53, 37, tzinfo=datetime.timezone.utc)
+        assert Event.zinoify_value(timestamp) == "1697630017"
+
+    def test_zinoify_value_when_value_is_anything_its_str_should_be_returned(self):
+        class Throwaway:
+            def __str__(self):
+                return "foo"
+
+        assert Event.zinoify_value(Throwaway()) == "foo"
 
 
 class TestDeviceState:
@@ -42,5 +76,5 @@ class TestDeviceStates:
 
 
 @pytest.fixture
-def fake_event():
-    yield ReachabilityEvent(id=42, router="example-gw.example.org", state=EventState.OPEN)
+def fake_event() -> ReachabilityEvent:
+    return ReachabilityEvent(id=42, router="example-gw.example.org", state=EventState.OPEN)
