bc9af41797c42635235298146c9a7156f7be187e
Simon Oliver Tveit <simtve@uninett.no>
2023-10-18 15:23:23 +0200

    Update tests

--- tests/tasks/test_bfdtask.py
+++ tests/tasks/test_bfdtask.py
@@ -1,3 +1,6 @@
+import ipaddress
+from ipaddress import IPv4Address
+
 import pytest
 
 from zino.config.models import PollDevice
@@ -5,6 +8,98 @@ from zino.oid import OID
 from zino.state import ZinoState
 from zino.statemodels import BFDEvent, BFDSessState, BFDState, Port
 from zino.tasks.bfdtask import BFDTask
+
+
+class TestParseAddress:
+    def test_converts_bytes_to_correct_ipv6_address(self):
+        parsed_address = BFDTask._convert_address(
+            b"\x00\x00\x00\x00\x00\x00\x00\x00\00\x00\x00\x00\x00\x00\x00\x01",
+            "ipv6",
+        )
+        assert parsed_address == ipaddress.IPv6Address("::1")
+
+    def test_converts_bytes_to_correct_ipv4_address(self):
+        parsed_address = BFDTask._convert_address(b"\x7f\x00\x00\x01", "ipv4")
+        assert parsed_address == ipaddress.IPv4Address("127.0.0.1")
+
+    def test_fails_if_parsing_ipv4_as_ipv6(self):
+        with pytest.raises(ipaddress.AddressValueError):
+            BFDTask._convert_address(b"\x7f\x00\x00\x01", "ipv6")
+
+    def test_fails_if_parsing_ipv6_as_ipv4(self):
+        with pytest.raises(ipaddress.AddressValueError):
+            BFDTask._convert_address(
+                b"\x00\x00\x00\x00\x00\x00\x00\x00\00\x00\x00\x00\x00\x00\x00\x01",
+                "ipv4",
+            )
+
+    def test_fails_if_address_type_is_invalid(self):
+        with pytest.raises(ValueError):
+            BFDTask._convert_address("\x7f\x00\x00\x01", "invalid")
+
+
+class TestJuniper:
+    def test_parse_juniper_rows_creates_correct_bfd_state(self, bfd_task_juniper, bfd_state, device_port):
+        fake_sparsewalk_response = {
+            OID(f".{bfd_state.session_index}"): {
+                "jnxBfdSessIntfName": device_port.ifdescr,
+                "bfdSessState": "up",
+                "bfdSessDiscriminator": bfd_state.session_index,
+                "bfdSessAddr": "\x7f\x00\x00\x01",
+                "bfdSessAddrType": "ipv4",
+            },
+        }
+        parsed_rows = bfd_task_juniper._parse_juniper_rows(fake_sparsewalk_response)
+        state = parsed_rows.get(device_port.ifdescr)
+        assert state == bfd_state
+
+    @pytest.mark.asyncio
+    async def test_poll_juniper_returns_correct_ifdescr_to_state_mapping(
+        self, bfd_task_juniper, bfd_state, device_port
+    ):
+        result = await bfd_task_juniper._poll_juniper()
+        assert device_port.ifdescr in result
+        state = result.get(device_port.ifdescr)
+        assert state == bfd_state
+
+    @pytest.mark.asyncio
+    async def test_task_updates_state_correctly(self, bfd_task_juniper, device_port, bfd_state):
+        assert not device_port.bfd_state
+        await bfd_task_juniper.run()
+        assert device_port.bfd_state == bfd_state
+
+    @pytest.mark.asyncio
+    async def test_event_should_not_be_made_the_first_time_a_port_is_polled(self, bfd_task_juniper, device_port):
+        assert not device_port.bfd_state
+        await bfd_task_juniper.run()
+        assert device_port.bfd_state
+        event = bfd_task_juniper.state.events.get(
+            device_name=bfd_task_juniper.device.name,
+            port=device_port.ifindex,
+            event_class=BFDEvent,
+        )
+        assert not event
+
+    @pytest.mark.asyncio
+    async def test_state_changing_should_create_event(self, bfd_task_juniper, device_port, bfd_state):
+        down_state = BFDState(session_state=BFDSessState.DOWN, session_index=bfd_state.session_index)
+        device_port.bfd_state = down_state
+        await bfd_task_juniper.run()
+        assert device_port.bfd_state != down_state
+        event = bfd_task_juniper.state.events.get(
+            device_name=bfd_task_juniper.device.name, port=device_port.ifindex, event_class=BFDEvent
+        )
+        assert event
+
+    @pytest.mark.asyncio
+    async def test_state_not_changing_should_not_create_event(self, bfd_task_juniper, device_port, bfd_state):
+        device_port.bfd_state = bfd_state
+        await bfd_task_juniper.run()
+        assert device_port.bfd_state == bfd_state
+        event = bfd_task_juniper.state.events.get(
+            device_name=bfd_task_juniper.device.name, port=device_port.ifindex, event_class=BFDEvent
+        )
+        assert not event
 
 
 @pytest.fixture()
@@ -16,11 +111,16 @@ def device_port():
 @pytest.fixture()
 def bfd_state():
     """Represents the BFD state at the simulated device"""
-    yield BFDState(session_state=BFDSessState.UP, session_index=4524)
+    yield BFDState(
+        session_state=BFDSessState.UP,
+        session_index=4524,
+        session_discr=4524,
+        session_addr=IPv4Address("127.0.0.1"),
+    )
 
 
 @pytest.fixture()
-def bfd_task(snmpsim, snmp_test_port, device_port):
+def bfd_task_juniper(snmpsim, snmp_test_port, device_port):
     device = PollDevice(
         name="buick.lab.example.org",
         address="127.0.0.1",
@@ -36,62 +136,3 @@ def bfd_task(snmpsim, snmp_test_port, device_port):
     device_state.ports[device_port.ifindex] = device_port
     task = BFDTask(device, state)
     yield task
-
-
-class TestJuniper:
-    def test_parse_juniper_rows_creates_correct_bfd_state(self, bfd_task, bfd_state, device_port):
-        fake_sparsewalk_response = {
-            OID(f".{bfd_state.session_index}"): {
-                "jnxBfdSessIntfName": device_port.ifdescr,
-                "bfdSessState": "up",
-            },
-        }
-        parsed_rows = bfd_task._parse_juniper_rows(fake_sparsewalk_response)
-        state = parsed_rows.get(device_port.ifdescr)
-        assert state == bfd_state
-
-    @pytest.mark.asyncio
-    async def test_poll_juniper_returns_correct_ifdescr_to_state_mapping(self, bfd_task, bfd_state, device_port):
-        result = await bfd_task._poll_juniper()
-        assert device_port.ifdescr in result
-        state = result.get(device_port.ifdescr)
-        assert state == bfd_state
-
-    @pytest.mark.asyncio
-    async def test_task_updates_state_correctly(self, bfd_task, device_port, bfd_state):
-        assert not device_port.bfd_state
-        await bfd_task.run()
-        assert device_port.bfd_state == bfd_state
-
-    @pytest.mark.asyncio
-    async def test_event_should_not_be_made_the_first_time_a_port_is_polled(self, bfd_task, device_port):
-        assert not device_port.bfd_state
-        await bfd_task.run()
-        assert device_port.bfd_state
-        event = bfd_task.state.events.get(
-            device_name=bfd_task.device.name,
-            port=device_port.ifindex,
-            event_class=BFDEvent,
-        )
-        assert not event
-
-    @pytest.mark.asyncio
-    async def test_state_changing_should_create_event(self, bfd_task, device_port, bfd_state):
-        down_state = BFDState(session_state=BFDSessState.DOWN, session_index=bfd_state.session_index)
-        device_port.bfd_state = down_state
-        await bfd_task.run()
-        assert device_port.bfd_state != down_state
-        event = bfd_task.state.events.get(
-            device_name=bfd_task.device.name, port=device_port.ifindex, event_class=BFDEvent
-        )
-        assert event
-
-    @pytest.mark.asyncio
-    async def test_state_not_changing_should_not_create_event(self, bfd_task, device_port, bfd_state):
-        device_port.bfd_state = bfd_state
-        await bfd_task.run()
-        assert device_port.bfd_state == bfd_state
-        event = bfd_task.state.events.get(
-            device_name=bfd_task.device.name, port=device_port.ifindex, event_class=BFDEvent
-        )
-        assert not event
