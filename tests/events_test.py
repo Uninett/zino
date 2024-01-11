e193a4e6eafee843e27ea786ef24057ba56ccf94
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-01-11 14:51:20 +0100

    Update all tests to use new commit workflow
    This updates all tests that were failing because they were not using
    the new checkout/commit paradigm for the event index.
    
    All tests must assume that anything returned from `Events.get_or_create`
    must be explicitly committed back to the index if they are new or
    changed.  Also, tests cannot assume that an object return by `get` or
    `get_or_create` is identical to an object the test is holding a
    reference to, since the `get/get_or_create` will create checkout
    copies.

--- tests/events_test.py
+++ tests/events_test.py
@@ -15,17 +15,18 @@ class TestEvents:
         events = Events()
         event = events.create_event("foobar", None, ReachabilityEvent)
 
-        assert event.id > 0
+        assert isinstance(event, ReachabilityEvent)
 
-    def test_event_registry_should_contain_one_event_when_first_event_is_created(self):
+    def test_event_registry_should_not_contain_newly_created_event(self):
         events = Events()
         events.create_event("foobar", None, ReachabilityEvent)
 
-        assert len(events) == 1
+        assert len(events) == 0
 
     def test_adding_two_identical_events_should_raise(self):
         events = Events()
-        events.create_event("foobar", None, ReachabilityEvent)
+        event = events.create_event("foobar", None, ReachabilityEvent)
+        events.commit(event)
 
         with pytest.raises(EventExistsError):
             events.create_event("foobar", None, ReachabilityEvent)
@@ -33,34 +34,38 @@ class TestEvents:
     def test_event_should_be_gettable_by_id(self):
         events = Events()
         event = events.create_event("foobar", None, ReachabilityEvent)
+        events.commit(event)
 
         assert events[event.id] == event
 
     def test_get_or_create_event_should_return_new_event(self):
         events = Events()
-        event, created = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        event = events.get_or_create_event("foobar", None, ReachabilityEvent)
 
-        assert created
+        assert event.id is None
         assert isinstance(event, Event)
 
     def test_get_or_create_event_should_return_existing_event_on_same_index(self):
         events = Events()
-        event1, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
-        event2, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        event1 = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        events.commit(event1)
+        event2 = events.get_or_create_event("foobar", None, ReachabilityEvent)
 
-        assert event2 is event1
+        assert event2 == event1
 
     def test_checkout_should_return_copy(self):
         events = Events()
-        original_event, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        original_event = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        events.commit(original_event)
         copy = events.checkout(original_event.id)
 
         assert original_event is not copy
 
     def test_checkout_should_return_deep_log_copy(self):
         events = Events()
-        original_event, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        original_event = events.get_or_create_event("foobar", None, ReachabilityEvent)
         original_event.add_log("first log")
+        events.commit(original_event)
 
         copy = events.checkout(original_event.id)
         copy.add_log("second log")
@@ -69,17 +74,19 @@ class TestEvents:
 
     def test_commit_should_replace_event(self):
         events = Events()
-        original_event, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        original_event = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        events.commit(original_event)
+
         copy = events.checkout(original_event.id)
         copy.add_log("this is the successor event")
 
         events.commit(copy)
 
-        assert events.get("foobar", None, ReachabilityEvent) is copy
+        assert events[copy.id] is copy
 
     def test_commit_should_open_embryonic_event(self):
         events = Events()
-        event, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        event = events.get_or_create_event("foobar", None, ReachabilityEvent)
         assert event.state == EventState.EMBRYONIC
         events.commit(event)
         assert event.state == EventState.OPEN
@@ -88,7 +95,7 @@ class TestEvents:
         events = Events()
         observer = Mock()
         events.add_event_observer(observer.observe)
-        event, _ = events.get_or_create_event("foobar", None, ReachabilityEvent)
+        event = events.get_or_create_event("foobar", None, ReachabilityEvent)
 
         events.commit(event)
         assert observer.observe.called
--- tests/tasks/test_reachabletask.py
+++ tests/tasks/test_reachabletask.py
@@ -59,24 +59,33 @@ class TestReachableTask:
         task = reachable_task
         event = task.state.events.create_event(task.device.name, None, ReachabilityEvent)
         event.reachability = ReachabilityState.NORESPONSE
+        task.state.events.commit(event)
+
         assert (await task.run()) is None
-        assert event.reachability == ReachabilityState.REACHABLE
+        updated_event = task.state.events[event.id]
+        assert updated_event.reachability == ReachabilityState.REACHABLE
 
     @pytest.mark.asyncio
     async def test_run_should_update_event_to_noresponse_when_device_is_unreachable(self, unreachable_task):
         task = unreachable_task
         event = task.state.events.create_event(task.device.name, None, ReachabilityEvent)
         event.reachability = ReachabilityState.REACHABLE
+        task.state.events.commit(event)
+
         assert (await task.run()) is None
-        assert event.reachability == ReachabilityState.NORESPONSE
+        updated_event = task.state.events[event.id]
+        assert updated_event.reachability == ReachabilityState.NORESPONSE
 
     @pytest.mark.asyncio
     async def test_run_extra_job_should_update_event_to_reachable_when_device_is_reachable(self, reachable_task):
         task = reachable_task
         event = task.state.events.create_event(task.device.name, None, ReachabilityEvent)
         event.reachability = ReachabilityState.NORESPONSE
+        task.state.events.commit(event)
+
         assert (await task._run_extra_job()) is None
-        assert event.reachability == ReachabilityState.REACHABLE
+        updated_event = task.state.events[event.id]
+        assert updated_event.reachability == ReachabilityState.REACHABLE
 
     @pytest.mark.asyncio
     async def test_run_extra_job_should_not_update_event_when_device_is_unreachable(self, unreachable_task):
