38fb6a966d6bc7c989028d78b1e69d2c54e17eea
Johanna England <johanna.england@sikt.no>
2024-01-15 13:33:00 +0100

    Add tests for fixup_ip_address

--- tests/tasks/test_bgpstatemonitortask.py
+++ tests/tasks/test_bgpstatemonitortask.py
@@ -1,3 +1,5 @@
+from ipaddress import IPv4Address, IPv6Address
+
 import pytest
 
 from zino.config.models import PollDevice
@@ -53,6 +55,32 @@ class TestGetLocalAs:
     @pytest.mark.parametrize("task", ["public"], indirect=True)
     async def test_get_local_as_returns_none_for_non_existent_local_as_with_juniper_bgp_style(self, task):
         assert (await task._get_local_as(bgp_style=BGPStyle.JUNIPER)) is None
+
+
+class TestFixupIPAddress:
+    @pytest.mark.parametrize("task", ["general-bgp"], indirect=True)
+    def test_can_parse_ipv4_starting_with_0x(self, task):
+        encoded_address = "0x7f000001"
+        decoded_address = task._fixup_ip_address(encoded_address)
+        assert decoded_address == IPv4Address("127.0.0.1")
+
+    @pytest.mark.parametrize("task", ["general-bgp"], indirect=True)
+    def test_can_parse_ipv6_starting_with_0x(self, task):
+        encoded_address = "0x13c7db1c4430c8266333aed0e6053a3b"
+        decoded_address = task._fixup_ip_address(encoded_address)
+        assert decoded_address == IPv6Address("13c7:db1c:4430:c826:6333:aed0:e605:3a3b")
+
+    @pytest.mark.parametrize("task", ["general-bgp"], indirect=True)
+    def test_can_parse_ipv4_in_string_format(self, task):
+        string_address = "127.0.0.1"
+        decoded_address = task._fixup_ip_address(string_address)
+        assert decoded_address == IPv4Address(string_address)
+
+    @pytest.mark.parametrize("task", ["general-bgp"], indirect=True)
+    def test_can_parse_ipv6_in_string_format(self, task):
+        string_address = "13c7:db1c:4430:c826:6333:aed0:e605:3a3b"
+        decoded_address = task._fixup_ip_address(string_address)
+        assert decoded_address == IPv6Address(string_address)
 
 
 @pytest.fixture
