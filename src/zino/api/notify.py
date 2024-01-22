7536a7bbe6228b8963bc4dd6be24674a8d8d51bd
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-01-22 12:00:38 +0100

    Add methods to build and send notifications
    This adds class methods to `Zino1NotificationProtocol` to build a
    list of notifications of event changes and send these notifications
    to all connected and tied notification channels.

--- src/zino/api/notify.py
+++ src/zino/api/notify.py
@@ -6,10 +6,11 @@ the server.
 """
 import asyncio
 import logging
-from typing import TYPE_CHECKING, Any, NamedTuple, Optional
+from typing import TYPE_CHECKING, Any, Iterator, NamedTuple, Optional
 
 from zino.api import auth
 from zino.state import ZinoState
+from zino.statemodels import Event, EventState
 
 if TYPE_CHECKING:
     from zino.api.legacy import Zino1ServerProtocol
@@ -79,3 +80,38 @@ class Zino1NotificationProtocol(asyncio.Protocol):
     def _respond_raw(self, message: str):
         """Encodes and sends a response line to the connected client"""
         self.transport.write(f"{message}\r\n".encode("utf-8"))
+
+    @classmethod
+    def build_and_send_notifications(
+        cls, server: "ZinoServer", new_event: Event, old_event: Optional[Event] = None
+    ) -> None:
+        """Prepares and sends notifications for all changes between old_event and new_event to all connected and tied
+        notification channels.
+        """
+        notifications = list(cls.build_notifications(new_event, old_event))
+        tied_channels = [channel for channel in server.notification_channels.values() if channel.tied_to]
+        _logger.debug("Sending %s notifications to %s tied channels", len(notifications), len(tied_channels))
+
+        for notification in notifications:
+            for channel in tied_channels:
+                channel.notify(notification)
+
+    @classmethod
+    def build_notifications(cls, new_event: Event, old_event: Optional[Event] = None) -> Iterator[Notification]:
+        """Generates a sequence of Notification objects from the changes detected between old_event and new_event.
+
+        If `old_event` is `None`, it is assumed the event is brand new, and only the state change from EMBRYONIC
+        matters.
+        """
+        changed = new_event.get_changed_fields(old_event) if old_event else ["state"]
+
+        for attr in changed:
+            if attr == "state":
+                old_state = EventState.EMBRYONIC if not old_event else old_event.state
+                yield Notification(new_event.id, attr, f"{old_state.value} {new_event.state.value}")
+
+            elif attr in ("log", "history"):
+                yield Notification(new_event.id, attr, 1)
+
+            else:
+                yield Notification(new_event.id, "attr", attr)
