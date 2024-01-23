d48c6a7851958cc59944c518de07d0756d4e1e16
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-01-23 11:49:24 +0100

    Test cancellation of remaining tasks
    This test ensures that when one task in the queue raises
    `DeviceUnreachable`, the remaining tasks on the list should not be run.

--- tests/tasks/test_run_all_tasks.py
+++ tests/tasks/test_run_all_tasks.py
@@ -1,13 +1,29 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
+from zino.state import ZinoState
 from zino.tasks import run_all_tasks, run_registered_tasks
 from zino.tasks.errors import DeviceUnreachableError
+from zino.tasks.task import Task
 
 
 class TestRunAllTasks:
     @pytest.mark.asyncio
     async def test_does_not_raise_error_if_device_is_unreachable(self, unreachable_task):
         assert (await run_all_tasks(unreachable_task.device, unreachable_task.state)) is None
+
+    @pytest.mark.asyncio
+    async def test_when_one_task_raises_device_unreachable_it_should_not_run_further_tasks(
+        self, raising_task, non_raising_task
+    ):
+        mock_device = Mock()
+        state = ZinoState()
+        with patch("zino.tasks.get_registered_tasks") as get_registered_tasks:
+            get_registered_tasks.return_value = [raising_task, non_raising_task]
+            await run_all_tasks(mock_device, state)
+            assert raising_task.was_run
+            assert not non_raising_task.was_run
 
 
 class TestRunRegisteredTasks:
@@ -16,3 +32,27 @@ class TestRunRegisteredTasks:
         """The rest of the registered tasks are cancelled as a result of this"""
         with pytest.raises(DeviceUnreachableError):
             await run_registered_tasks(unreachable_task.device, unreachable_task.state)
+
+
+@pytest.fixture()
+def raising_task():
+    class RaisingTask(Task):
+        was_run = False
+
+        async def run(self):
+            RaisingTask.was_run = True
+            raise DeviceUnreachableError("just testing")
+
+    return RaisingTask
+
+
+@pytest.fixture()
+def non_raising_task():
+    class NonRaisingTask(Task):
+        was_run = False
+
+        async def run(self):
+            NonRaisingTask.was_run = True
+            return True
+
+    return NonRaisingTask
