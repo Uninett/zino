46f464b64f72d40f5e8260ba21c2b635fd22c769
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-03-19 14:38:23 +0100

    Expand docstrings.

--- src/zino/trapd.py
+++ src/zino/trapd.py
@@ -10,7 +10,13 @@ _logger = logging.getLogger(__name__)
 
 
 class TrapReceiver:
-    """Zino Adapter for SNMP trap reception using PySNMP"""
+    """Zino Adapter for SNMP trap reception using PySNMP.
+
+    A major difference to Zino 1 is that this receiver must explicitly be configured with SNMP community strings that
+    will be accepted.  Zino 1 accepts traps with any community string, as long as their origin is any one of the
+    devices configured in `polldevs.cf`.  However, PySNMP places heavy emphasis on being standards compliant,
+    and will not even pass on traps to our callbacks unless they match the authorization config for the SNMP engine.
+    """
 
     def __init__(self, address: str = "0.0.0.0", port: int = 162, loop=None):
         self.transport = None
@@ -21,6 +27,7 @@ class TrapReceiver:
         self._communities = set()
 
     async def open(self):
+        """Opens the UDP transport socket and starts receiving traps"""
         self.transport = udp.UdpTransport(loop=self.loop).openServerMode((self.address, self.port))
         # This attribute needs to be awaited to ensure the socket is really opened,
         # unsure of why PySNMP doesn't do this, or how else it's supposed to be achieved
@@ -39,6 +46,7 @@ class TrapReceiver:
         config.addV1System(self.snmp_engine, str(len(self._communities)), community)
 
     def trap_received(self, snmp_engine, state_reference, context_engine_id, context_name, var_binds, callback_context):
+        """Callback function that receives all matched trap messages on PySNMP's incoming transport socket"""
         _logger.info(
             'Trap from ContextEngineId "%s", ContextName "%s"',
             context_engine_id.prettyPrint(),
