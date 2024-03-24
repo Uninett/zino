832bf01e5fc2b0ce060340d3e28e06665f0e15e1
Simon Oliver Tveit <simtve@uninett.no>
2024-03-24 06:20:59 +0100

    Define PM loop func under PM state model

--- src/zino/planned_maintenance.py
+++ src/zino/planned_maintenance.py
@@ -4,7 +4,7 @@ from typing import Dict, Literal, Optional, Protocol
 
 from pydantic.main import BaseModel
 
-from zino.statemodels import Event, PlannedMaintenance
+from zino.statemodels import Event, EventState, PlannedMaintenance
 
 _log = logging.getLogger(__name__)
 
@@ -98,6 +98,42 @@ class PlannedMaintenances(BaseModel):
     def _call_observers(self) -> None:
         for observer in self._observers:
             observer()
+
+    def periodic(self):
+        now = datetime.datetime.now()
+
+        started_maintenances = self.get_started_planned_maintenances(now=now)
+        for maintenance in started_maintenances:
+            events = self._get_or_create_maintenance_events_for_maintenance_start(maintenance=maintenance)
+            for event in events:
+                event.state = EventState.IGNORED
+                self.state.events.commit(event)
+                maintenance.pm_events.append(event)
+
+        # Get all events (maybe already filter here for ignored and closed events?)
+        events = []
+        for event in events:
+            for active_pm in self.get_active_planned_maintenances(now=now):
+                if matches_planned_maintenance(event=event, maintenance=active_pm):
+                    event.state = EventState.IGNORED
+                    self.state.events.commit(event)
+                    active_pm.pm_events.append(event)
+
+        ended_maintenances = self.get_ended_planned_maintenances(now=now)
+        for maintenance in ended_maintenances:
+            for event in maintenance.pm_events:
+                if event.state is not EventState.OPEN:
+                    # This is how it is currently done in Zino 1.0
+                    # Could use some improvement on detecting the actual correct state
+                    event.state = EventState.OPEN
+                    self.state.events.commit(event)
+
+    def _get_or_create_maintenance_events_for_maintenance_start(self, maintenance: PlannedMaintenance) -> list[Event]:
+        """Creates/gets events that are affected by the given starting planned
+        maintenance
+        """
+        # See `start_pm` function in Zino 1.0 `pm.tcl`
+        pass
 
 
 def matches_planned_maintenance(event: Event, maintenance: PlannedMaintenance) -> bool:
