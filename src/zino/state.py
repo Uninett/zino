00bedc861b36647852d79f54a98e4a538ac1504c
Johanna England <johanna.england@sikt.no>
2024-05-30 13:58:15 +0200

    Use persistence.file for file of state dumps/loads

--- src/zino/state.py
+++ src/zino/state.py
@@ -14,7 +14,6 @@ from zino.planned_maintenance import PlannedMaintenances
 from zino.statemodels import DeviceStates
 
 _log = logging.getLogger(__name__)
-STATE_FILENAME = "zino-state.json"
 
 # Dictionary of configured devices
 polldevs: Dict[str, PollDevice] = {}
@@ -33,14 +32,14 @@ class ZinoState(BaseModel):
     addresses: dict[IPAddress, str] = {}
     planned_maintenances: PlannedMaintenances = Field(default_factory=PlannedMaintenances)
 
-    def dump_state_to_file(self, filename: str = STATE_FILENAME):
+    def dump_state_to_file(self, filename: str):
         """Dumps the full state to a file in JSON format"""
         _log.debug("dumping state to %s", filename)
         with open(filename, "w") as statefile:
             statefile.write(self.model_dump_json(exclude_none=True, indent=2))
 
     @classmethod
-    def load_state_from_file(cls, filename: str = STATE_FILENAME) -> Optional["ZinoState"]:
+    def load_state_from_file(cls, filename: str) -> Optional["ZinoState"]:
         """Loads and returns a previously persisted ZinoState from a JSON file dump.
 
         :returns: A ZinoState object if the state file was found, None if it wasn't.  If the state file is invalid or
