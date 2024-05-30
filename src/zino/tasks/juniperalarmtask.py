0bf46a5109f882a5e58ca7f1f86bff6e61370b19
Johanna England <johanna.england@sikt.no>
2024-05-30 11:24:36 +0200

    Catch NoSuchNameError when getting alarm count

--- src/zino/tasks/juniperalarmtask.py
+++ src/zino/tasks/juniperalarmtask.py
@@ -1,5 +1,6 @@
 import logging
 
+from zino.snmp import NoSuchNameError
 from zino.statemodels import AlarmEvent, AlarmType
 from zino.tasks.task import Task
 
@@ -15,7 +16,7 @@ class JuniperAlarmTask(Task):
 
         try:
             yellow_alarm_count, red_alarm_count = await self._get_juniper_alarms()
-        except TypeError:
+        except (TypeError, NoSuchNameError):
             return
 
         if not self.device_state.alarms:
