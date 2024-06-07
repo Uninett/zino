2c3bd5d6dc072afcb0cd1835f972e6c075a6d7ea
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-06-07 17:52:19 +0200

    Implement a flap aging job
    This roughly follows the Zino 1 implementation, but is not yet
    feature complete.

--- src/zino/flaps.py
+++ src/zino/flaps.py
@@ -3,12 +3,22 @@
 Flapping is normally only tracked/updated based on incoming link traps.
 """
 
+import logging
 from datetime import datetime
-from typing import Optional, Tuple
+from typing import TYPE_CHECKING, List, Optional, Tuple
 
 from pydantic import BaseModel, Field
 
+from zino.config.models import PollDevice
+from zino.statemodels import FlapState, InterfaceState, PortStateEvent
+from zino.tasks.linkstatetask import LinkStateTask
+
+if TYPE_CHECKING:
+    from zino.state import ZinoState
+
 from zino.time import now
+
+_logger = logging.getLogger(__name__)
 
 # Constants from Zino 1
 FLAP_THRESHOLD = 35
@@ -110,4 +120,63 @@ class FlappingStates(BaseModel):
         return self.interfaces[interface].hist_val
 
 
-# TODO: Implement a flap ager job that ages all flapping states every FLAP_DECR_INTERVAL seconds
+# TODO: Ensure age_flapping_states() is called every FLAP_DECREMENT_INTERVAL_SECONDS
+async def age_flapping_states(state: ZinoState, polldevs: dict[str, PollDevice]):
+    """Ages all flapping states in the given ZinoState.  Should be called every FLAP_DECREMENT_INTERVAL_SECONDS."""
+    for index, flap in state.flapping.interfaces.items():
+        await age_single_interface_flapping_state(flap, index, state, polldevs)
+
+
+async def age_single_interface_flapping_state(
+    flap: FlappingState, index: PortIndex, state: ZinoState, polldevs: dict[str, PollDevice]
+) -> None:
+    flap.age()
+    router, ifindex = index
+    polldev = polldevs.get(router)
+    port = state.devices.get(router).ports.get(ifindex) if router in state.devices else None
+    if flap.hist_val < FLAP_MIN:
+        if state.flapping.was_flapping(index):
+            if port:
+                msg = f'{router}: intf "{port.ifdescr}" ix {ifindex} stopped flapping (aging)'
+            else:
+                msg = f"{router}: ix {ifindex} stopped flapping (aging)"
+            _logger.info(msg)
+
+            # TODO: At this point, Zino 1 looks both for matching events that are closed and that are open.
+            #       Zino 2 doesn't index closed events, so we either need to implement that, or search for them.
+            #       Either way, we need to clarify why closed events need to be updated (which sounds strange to me)
+            events: List[PortStateEvent] = [state.events.get_or_create_event(router, ifindex, PortStateEvent)]
+            for event in events:
+                # If we lack information, revisit later
+                if not polldev:
+                    continue
+                if not polldev.address:
+                    continue
+                if not polldev.priority:
+                    continue
+                if not port:
+                    continue
+
+                event.flapstate = FlapState.STABLE
+                event.flaps = state.flapping.get_flap_count(index)
+                event.port = port.ifdescr
+                event.portstate = port.state
+                event.router = router
+                event.polladdr = polldev.address
+                event.priority = polldev.priority
+                event.ifindex = ifindex
+                event.descr = port.ifalias
+
+                event.add_log(msg)
+
+                state.events.commit(event)
+
+            old_state = port.state
+            port.state = InterfaceState.FLAPPING
+            poll = LinkStateTask(device=polldev, state=state)
+            try:
+                await poll.poll_single_interface(ifindex)
+            except Exception:  # noqa
+                port.state = old_state
+
+        state.flapping.unflap(index)
