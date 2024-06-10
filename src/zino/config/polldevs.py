6b17b4945681927ba91c2b8029c1d464213c8b51
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-06-10 11:45:51 +0200

    Improve error tracing in `polldevs.cf` validation
    This adds filename, line number and device block information to
    polldevs parser exceptions in two steps, so that user can more readily
    identify where in `polldevs.cf` their problem is.
    
    The line parser can now identify which lines failed to parse.
    
    Device blocks are identified by their starting line number (and name
    attribute, if available), and the first Pydantic validation error for
    each block is crudely translated to a Zino `InvalidConfiguration`
    exception.

--- src/zino/config/polldevs.py
+++ src/zino/config/polldevs.py
@@ -1,6 +1,8 @@
 """Functionality to parse and validate the legacy polldevs.cf config file"""
 
-from typing import Iterator, TextIO
+from typing import Iterator, TextIO, Tuple
+
+from pydantic import ValidationError
 
 from zino.config.models import PollDevice
 
@@ -12,34 +14,53 @@ def read_polldevs(filename: str) -> Iterator[PollDevice]:
     multiple spaces in value assignments.
     """
     defaults = {}
-    with open(filename, "r") as devs:
-        for section in _read_conf_sections(devs):
-            if _contains_defaults(section):
-                defaults.update(_parse_defaults(section))
-                continue
+    try:
+        with open(filename, "r") as devs:
+            for lineno, section in _read_conf_sections(devs):
+                if _contains_defaults(section):
+                    defaults.update(_parse_defaults(section))
+                    continue
 
-            yield PollDevice(**(defaults | section))
+                try:
+                    yield PollDevice(**(defaults | section))
+                except ValidationError as error:
+                    first_error = error.errors()[0]
+                    device_name = section.get("name", "N/A")
+                    attribute = first_error["loc"][0]
+                    raise InvalidConfiguration(
+                        f"Validation error in device block {device_name!r}: {first_error['msg']} ({attribute!r})",
+                        filename=filename,
+                        lineno=lineno,
+                    )
+
+    except InvalidConfiguration as error:
+        error.filename = filename
+        raise
 
 
-def _read_conf_sections(filehandle: TextIO) -> Iterator[dict]:
+def _read_conf_sections(filehandle: TextIO) -> Iterator[Tuple[int, dict]]:
     """Reads individual configuration sections from `polldevs.cf`, yielding each one as a separate dict"""
     section = {}
-    for line in filehandle:
+    first_line = None
+    for lineno, line in enumerate(filehandle):
+        if not first_line:
+            first_line = lineno + 1
         line = line.strip()
         if line.startswith("#"):
             continue
         if not line:
             if section:
-                yield section
+                yield first_line, section
                 section = {}
+                first_line = None
             continue
         try:
             key, value = line.split(":", maxsplit=1)
         except ValueError:
-            raise InvalidConfiguration(f"{line!r} is not a valid configuration line")
+            raise InvalidConfiguration(f"{line!r} is not a valid configuration line", lineno=lineno + 1)
         section[key.strip()] = value.strip()
     if section:
-        yield section
+        yield first_line, section
 
 
 def _contains_defaults(section: dict) -> bool:
@@ -51,4 +72,14 @@ def _parse_defaults(section: dict) -> dict:
 
 
 class InvalidConfiguration(Exception):
-    pass
+    def __init__(self, message=None, filename=None, lineno=None):
+        self.filename = filename
+        self.lineno = lineno
+        super().__init__(message)
+
+    def __str__(self):
+        location = [item for item in (self.filename, self.lineno) if item]
+        if location:
+            location = ":".join(str(item) for item in location)
+            return f"{location}: {super().__str__()}"
+        return super().__str__()
