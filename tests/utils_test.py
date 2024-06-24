665177b82f15391dc62c3488def66c51d6ec3b79
Simon Oliver Tveit <simon.tveit@sikt.no>
2024-06-24 17:24:23 +0200

    Update utils tests

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,7 +1,7 @@
-import socket
 from ipaddress import IPv4Address, IPv6Address
-from unittest.mock import patch
+from unittest.mock import AsyncMock, MagicMock
 
+import aiodns
 import pytest
 
 from zino.utils import parse_ip, reverse_dns
@@ -48,17 +48,38 @@ class TestParseIP:
 
 
 class TestReverseDNS:
-    @patch("zino.utils.socket.gethostbyaddr")
-    def test_should_return_reverse_dns_for_valid_ip(self, mock_gethostbyaddr):
+    @pytest.mark.asyncio
+    async def test_should_return_reverse_dns_for_valid_ip(self, mock_dnsresolver):
         valid_ip = "8.8.8.8"
         reverse_dns_value = "reverse.dns.example.com"
-        mock_gethostbyaddr.return_value = [reverse_dns_value, [], [valid_ip]]
-        result = reverse_dns(valid_ip)
+
+        # Create mock for return value of gethostbyaddr
+        mock_gethostbyaddr_return_value = MagicMock()
+        mock_gethostbyaddr_return_value.configure_mock(name=reverse_dns_value, aliases=[], addresses=[valid_ip])
+
+        # Create gethostbyaddr async mock
+        mock_gethostbyaddr = AsyncMock(return_value=mock_gethostbyaddr_return_value)
+
+        # Mock DNSRsolver.gethostbyaddr function
+        mock_dnsresolver.gethostbyaddr = mock_gethostbyaddr
+
+        result = await reverse_dns(valid_ip)
         assert result == reverse_dns_value
 
-    @patch("zino.utils.socket.gethostbyaddr")
-    def test_should_return_none_for_invalid_ip(self, mock_gethostbyaddr):
+    @pytest.mark.asyncio
+    async def test_should_return_none_for_invalid_ip(self, mock_dnsresolver):
         invalid_ip = "0.0.0.0"
-        mock_gethostbyaddr.side_effect = socket.herror
-        result = reverse_dns(invalid_ip)
+
+        # Mock DNSResolver.gethostbyaddr to raise DNSError
+        mock_gethostbyaddr = AsyncMock(side_effect=aiodns.error.DNSError)
+        mock_dnsresolver.gethostbyaddr = mock_gethostbyaddr
+
+        result = await reverse_dns(invalid_ip)
         assert result is None
+
+
+@pytest.fixture
+def mock_dnsresolver(monkeypatch) -> AsyncMock:
+    mock_dnsresolver = AsyncMock()
+    monkeypatch.setattr("zino.utils.aiodns.DNSResolver", lambda loop: mock_dnsresolver)
+    return mock_dnsresolver
