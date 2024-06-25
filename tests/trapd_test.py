9a1bb45ac53b5445b38c13ec871b3a34f7736882
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-06-25 15:30:57 +0200

    Add docstring to explain helper function
    I see no reason to delve into the actual details of how to supply
    the snmptrapd command line program with trap varbinds, those details
    are best left to the man page of the program itself.

--- tests/trapd_test.py
+++ tests/trapd_test.py
@@ -163,6 +163,11 @@ class TestTrapReceiverExternally:
 
 
 async def send_trap_externally(*args: str):
+    """Uses the snmptrap command line program to send a trap to the local trap receiver test instance on port 1162.
+
+    :param args: The arguments to pass to the snmptrap command line program, see `man snmptrap` for details on the
+                 rather esoteric syntax.
+    """
     args = " ".join(args)
     proc = await asyncio.create_subprocess_shell(f"snmptrap -v 2c -c public localhost:1162 '' {args}")
     await proc.communicate()
