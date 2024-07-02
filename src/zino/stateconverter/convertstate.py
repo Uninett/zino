80fe97970df462a50c05e3ffe8ef2f6c908da607
Simon Oliver Tveit <simtve@uninett.no>
2024-07-02 15:16:58 +0200

    Add func for reading old state

--- src/zino/stateconverter/convertstate.py
+++ src/zino/stateconverter/convertstate.py
@@ -2,10 +2,40 @@ import argparse
 import logging
 
 from zino.state import ZinoState
+from zino.stateconverter.linedata import LineData, get_line_data
 
 
 def create_state(old_state_file: str) -> ZinoState:
     pass
+
+
+def load_state_to_dict(file: str) -> dict[str, list[LineData]]:
+    state_dict = {}
+    lines = read_file_lines(file)
+    for line in lines:
+        # these lines do not contain any information
+        if not line.startswith("set"):
+            continue
+        linedata = get_line_data(line)
+        var_name = get_var_name(line)
+        if var_name not in state_dict:
+            state_dict[var_name] = []
+        state_dict[var_name].append(linedata)
+    return state_dict
+
+
+def get_var_name(line) -> str:
+    split_line = line.split()
+    var = split_line[1].split("(")[0]
+    if "::EventAttrs_" in var:
+        var = var.split("_")[0]
+    return var
+
+
+def read_file_lines(file: str):
+    with open(file, "r", encoding="latin-1") as state_file:
+        lines = state_file.read().splitlines()
+    return lines
 
 
 def get_parser():
