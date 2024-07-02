75c6801cbc691c5fbaa85a638638d1ede86bcd5f
Simon Oliver Tveit <simtve@uninett.no>
2024-07-02 15:48:24 +0200

    Move file reading to utils

--- src/zino/stateconverter/utils.py
+++ src/zino/stateconverter/utils.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from ipaddress import ip_address
 
-from zino.stateconverter.linedata import LineData
+from zino.stateconverter.linedata import LineData, get_line_data
 from zino.statemodels import IPAddress
 
 OldState = defaultdict[str, list[LineData]]
@@ -16,3 +16,30 @@ def parse_ip(ip: str) -> IPAddress:
             return ip_address(ip)
         else:
             raise
+
+
+def load_state_to_dict(file: str) -> OldState:
+    state_dict = defaultdict(list)
+    lines = _read_file_lines(file)
+    for line in lines:
+        # these lines do not contain any information
+        if not line.startswith("set"):
+            continue
+        linedata = get_line_data(line)
+        var_name = _get_var_name(line)
+        state_dict[var_name].append(linedata)
+    return state_dict
+
+
+def _get_var_name(line) -> str:
+    split_line = line.split()
+    var = split_line[1].split("(")[0]
+    if "::EventAttrs_" in var:
+        var = var.split("_")[0]
+    return var
+
+
+def _read_file_lines(file: str):
+    with open(file, "r", encoding="latin-1") as state_file:
+        lines = state_file.read().splitlines()
+    return lines
