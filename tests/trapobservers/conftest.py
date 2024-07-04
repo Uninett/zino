06151d6d435aec5a7520bb86f9d61f5fdc28b13b
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-07-04 17:21:05 +0200

    Move useful test fixture conftest.py
    We want to re-use this for other trap tests.

---
+++ tests/trapobservers/conftest.py
@@ -0,0 +1,15 @@
+"""Common fixtures for trap tests"""
+
+import ipaddress
+
+import pytest
+
+from zino.statemodels import DeviceState
+from zino.trapd import TrapOriginator
+
+
+@pytest.fixture
+def localhost_trap_originator():
+    addr = ipaddress.IPv4Address("127.0.0.1")
+    device = DeviceState(name="localhost", addresses=set((addr,)))
+    return TrapOriginator(address=addr, port=162, device=device)
