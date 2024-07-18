b814b4f2d57a84e95d95cd91032d0a8b4e7e92b2
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-07-18 12:44:04 +0200

    Add CISCO-PIM-MIB dump

---
+++ src/zino/mibdumps/IANA-RTPROTO-MIB.py
@@ -0,0 +1,28 @@
+#
+# PySNMP MIB module IANA-RTPROTO-MIB (http://snmplabs.com/pysmi)
+# ASN.1 source file:///home/mvold/mibs/v2/ianaiprouteprotocol.mib
+# Produced by pysmi-0.3.4 at Thu Jul 18 12:37:03 2024
+# On host agrajag platform Linux version 6.6.37 by user mvold
+# Using Python version 3.11.9 (main, Apr  2 2024, 08:25:04) [GCC 13.2.0]
+#
+ObjectIdentifier, OctetString, Integer = mibBuilder.importSymbols("ASN1", "ObjectIdentifier", "OctetString", "Integer")
+NamedValues, = mibBuilder.importSymbols("ASN1-ENUMERATION", "NamedValues")
+ConstraintsIntersection, ConstraintsUnion, SingleValueConstraint, ValueSizeConstraint, ValueRangeConstraint = mibBuilder.importSymbols("ASN1-REFINEMENT", "ConstraintsIntersection", "ConstraintsUnion", "SingleValueConstraint", "ValueSizeConstraint", "ValueRangeConstraint")
+ModuleCompliance, NotificationGroup = mibBuilder.importSymbols("SNMPv2-CONF", "ModuleCompliance", "NotificationGroup")
+NotificationType, Integer32, Counter32, MibScalar, MibTable, MibTableRow, MibTableColumn, iso, Gauge32, IpAddress, Unsigned32, ObjectIdentity, TimeTicks, Counter64, Bits, MibIdentifier, ModuleIdentity, mib_2 = mibBuilder.importSymbols("SNMPv2-SMI", "NotificationType", "Integer32", "Counter32", "MibScalar", "MibTable", "MibTableRow", "MibTableColumn", "iso", "Gauge32", "IpAddress", "Unsigned32", "ObjectIdentity", "TimeTicks", "Counter64", "Bits", "MibIdentifier", "ModuleIdentity", "mib-2")
+DisplayString, TextualConvention = mibBuilder.importSymbols("SNMPv2-TC", "DisplayString", "TextualConvention")
+ianaRtProtoMIB = ModuleIdentity((1, 3, 6, 1, 2, 1, 84))
+ianaRtProtoMIB.setRevisions(('2000-09-26 00:00',))
+if mibBuilder.loadTexts: ianaRtProtoMIB.setLastUpdated('200009260000Z')
+if mibBuilder.loadTexts: ianaRtProtoMIB.setOrganization('IANA')
+class IANAipRouteProtocol(TextualConvention, Integer32):
+    status = 'current'
+    subtypeSpec = Integer32.subtypeSpec + ConstraintsUnion(SingleValueConstraint(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17))
+    namedValues = NamedValues(("other", 1), ("local", 2), ("netmgmt", 3), ("icmp", 4), ("egp", 5), ("ggp", 6), ("hello", 7), ("rip", 8), ("isIs", 9), ("esIs", 10), ("ciscoIgrp", 11), ("bbnSpfIgp", 12), ("ospf", 13), ("bgp", 14), ("idpr", 15), ("ciscoEigrp", 16), ("dvmrp", 17))
+
+class IANAipMRouteProtocol(TextualConvention, Integer32):
+    status = 'current'
+    subtypeSpec = Integer32.subtypeSpec + ConstraintsUnion(SingleValueConstraint(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+    namedValues = NamedValues(("other", 1), ("local", 2), ("netmgmt", 3), ("dvmrp", 4), ("mospf", 5), ("pimSparseDense", 6), ("cbt", 7), ("pimSparseMode", 8), ("pimDenseMode", 9), ("igmpOnly", 10), ("bgmp", 11), ("msdp", 12))
+
+mibBuilder.exportSymbols("IANA-RTPROTO-MIB", PYSNMP_MODULE_ID=ianaRtProtoMIB, IANAipMRouteProtocol=IANAipMRouteProtocol, ianaRtProtoMIB=ianaRtProtoMIB, IANAipRouteProtocol=IANAipRouteProtocol)
