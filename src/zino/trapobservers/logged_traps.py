2ed2feb6f03fbdf302c9da1b89f16863462801e8
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-07-18 13:01:20 +0200

    Log CISCO-PIM-MIB traps
    Logs `ciscoPimInvalidRegister` and `ciscoPimInvalidJoinPrune` just like
    Zino 1 does.

--- src/zino/trapobservers/logged_traps.py
+++ src/zino/trapobservers/logged_traps.py
@@ -1,6 +1,7 @@
 """This module implements custom logging of certain SNMP traps, but takes no other action"""
 
 import logging
+from ipaddress import ip_address
 from typing import Optional
 
 from zino.trapd import TrapMessage, TrapObserver
@@ -46,6 +47,51 @@ class CiscoConfigManEventLogger(TrapObserver):
                     "ccmHistoryEventConfigDestination",
                 )
             ],
+        )
+
+        return False  # stop trap processing here
+
+
+class CiscoPimTrapLogger(TrapObserver):
+    """Logs details of Cisco PIM traps.
+
+    Example of manually generating a matching trap with test data:
+
+          snmptrap -v 2c -c public localhost:1162 "" \
+               CISCO-PIM-MIB::ciscoPimInvalidRegister \
+               CISCO-PIM-MIB::cpimLastErrorOriginType i 1 \
+               CISCO-PIM-MIB::cpimLastErrorOrigin x "0A000001" \
+               CISCO-PIM-MIB::cpimLastErrorGroupType i 1 \
+               CISCO-PIM-MIB::cpimLastErrorGroup x "0A000002" \
+               CISCO-PIM-MIB::cpimLastErrorRPType i 1 \
+               CISCO-PIM-MIB::cpimLastErrorRP  x "0A000003" \
+               CISCO-PIM-MIB::cpimInvalidRegisterMsgsRcvd c 42
+    """
+
+    WANTED_TRAPS = {
+        ("CISCO-PIM-MIB", "ciscoPimInvalidRegister"),
+        ("CISCO-PIM-MIB", "ciscoPimInvalidJoinPrune"),
+    }
+
+    async def handle_trap(self, trap: TrapMessage) -> Optional[bool]:
+        if "cpimLastErrorOriginType" not in trap:
+            return False
+
+        if trap.get_all("cpimLastErrorOriginType")[0].value != "ipv4":
+            return False
+
+        origin = ip_address(bytes(trap.get_all("cpimLastErrorOrigin")[0].raw_value))
+        group = ip_address(bytes(trap.get_all("cpimLastErrorGroup")[0].raw_value))
+        rp = ip_address(bytes(trap.get_all("cpimLastErrorRP")[0].raw_value))
+
+        trap_type = "register" if trap.name.endswith("Register") else "join-prune"
+        _logger.info(
+            "%s: PIM-invalid-%s: from %s group %s RP %s",
+            trap.agent.device.name,
+            trap_type,
+            origin,
+            group,
+            rp,
         )
 
         return False  # stop trap processing here
--- tests/trapobservers/logged_traps_test.py
+++ tests/trapobservers/logged_traps_test.py
@@ -6,6 +6,7 @@ import pytest
 from zino.trapd import TrapMessage
 from zino.trapobservers.logged_traps import (
     CiscoConfigManEventLogger,
+    CiscoPimTrapLogger,
     CiscoReloadTrapLogger,
     RestartTrapLogger,
 )
@@ -59,3 +60,69 @@ class TestCiscoConfigManEventLogger:
         with caplog.at_level(logging.INFO):
             await observer.handle_trap(trap=trap)
             assert "localhost: config-change: cmd-src snmp conf-src networkTftp dst startup" in caplog.text
+
+
+class TestCiscoPimTrapLogger:
+    @pytest.mark.asyncio
+    async def test_when_handle_trap_is_called_with_invalid_pim_register_it_should_log_it_correctly(
+        self, caplog, localhost_trap_originator
+    ):
+        observer = CiscoPimTrapLogger(state=Mock())
+        trap = TrapMessage(
+            agent=localhost_trap_originator,
+            mib="CISCO-PIM-MIB",
+            name="ciscoPimInvalidRegister",
+            variables=[
+                Mock(var="cpimLastErrorOriginType", value="ipv4"),
+                Mock(var="cpimLastErrorOrigin", raw_value=b"\x0A\x00\x00\x01"),
+                Mock(var="cpimLastErrorGroupType", value="ipv4"),
+                Mock(var="cpimLastErrorGroup", raw_value=b"\x0A\x00\x00\x02"),
+                Mock(var="cpimLastErrorRPType", value="ipv4"),
+                Mock(var="cpimLastErrorRP", raw_value=b"\x0A\x00\x00\x03"),
+                Mock(var="cpimInvalidRegisterMsgsRcvd", value=42),
+            ],
+        )
+        with caplog.at_level(logging.INFO):
+            await observer.handle_trap(trap=trap)
+            assert "localhost: PIM-invalid-register: from 10.0.0.1 group 10.0.0.2 RP 10.0.0.3" in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_when_trap_is_missing_error_origin_type_it_should_ignore_it(self, caplog, localhost_trap_originator):
+        observer = CiscoPimTrapLogger(state=Mock())
+        trap = TrapMessage(
+            agent=localhost_trap_originator,
+            mib="CISCO-PIM-MIB",
+            name="ciscoPimInvalidRegister",
+            variables=[
+                Mock(var="cpimLastErrorOrigin", raw_value=b"\x0A\x00\x00\x01"),
+                Mock(var="cpimLastErrorGroupType", value="ipv4"),
+                Mock(var="cpimLastErrorGroup", raw_value=b"\x0A\x00\x00\x02"),
+                Mock(var="cpimLastErrorRPType", value="ipv4"),
+                Mock(var="cpimLastErrorRP", raw_value=b"\x0A\x00\x00\x03"),
+                Mock(var="cpimInvalidRegisterMsgsRcvd", value=42),
+            ],
+        )
+        with caplog.at_level(logging.INFO):
+            await observer.handle_trap(trap=trap)
+            assert "PIM-invalid" not in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_when_trap_error_origin_type_is_not_ipv4_it_should_ignore_it(self, caplog, localhost_trap_originator):
+        observer = CiscoPimTrapLogger(state=Mock())
+        trap = TrapMessage(
+            agent=localhost_trap_originator,
+            mib="CISCO-PIM-MIB",
+            name="ciscoPimInvalidRegister",
+            variables=[
+                Mock(var="cpimLastErrorOriginType", value="ipv6"),
+                Mock(var="cpimLastErrorOrigin", raw_value=b"\x0A\x00\x00\x01"),
+                Mock(var="cpimLastErrorGroupType", value="ipv4"),
+                Mock(var="cpimLastErrorGroup", raw_value=b"\x0A\x00\x00\x02"),
+                Mock(var="cpimLastErrorRPType", value="ipv4"),
+                Mock(var="cpimLastErrorRP", raw_value=b"\x0A\x00\x00\x03"),
+                Mock(var="cpimInvalidRegisterMsgsRcvd", value=42),
+            ],
+        )
+        with caplog.at_level(logging.INFO):
+            await observer.handle_trap(trap=trap)
+            assert "PIM-invalid" not in caplog.text
