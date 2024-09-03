21f7f7d505cdf151820694fc434d435474976f62
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-09-03 12:23:49 +0200

    Resolve pytest warning for `TestEnum`
    Pytest views the class `TestEnum` as a potential test class to collect
    and run, because its name starts with the word `Test` - but since it
    is not a test class and inherits a `__init__` constructor, it just
    outputs a warning instead.  Renaming the class is the easiest fix.

--- tests/compat_test.py
+++ tests/compat_test.py
@@ -7,15 +7,15 @@ from zino.compat import StrEnum
 
 @pytest.mark.skipif(sys.version_info >= (3, 11), reason="StrEnum compat is only valid for Python < 3.11")
 def test_strenum_value_should_be_string():
-    assert str(TestEnum.NOT_PRESENT) == "notPresent"
+    assert str(StrTestEnum.NOT_PRESENT) == "notPresent"
 
 
 @pytest.mark.skipif(sys.version_info >= (3, 11), reason="StrEnum compat is only valid for Python < 3.11")
 def test_strenum_should_be_able_to_instantiate_from_string():
-    assert TestEnum("notPresent") == TestEnum.NOT_PRESENT
+    assert StrTestEnum("notPresent") == StrTestEnum.NOT_PRESENT
 
 
-class TestEnum(StrEnum):
+class StrTestEnum(StrEnum):
     UP = "up"
     DOWN = "down"
     NOT_PRESENT = "notPresent"
