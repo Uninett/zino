d7acd04bb93e53a2cd950388fd64492ba4e1637c
Morten Brekkevold <morten.brekkevold@sikt.no>
2025-01-22 11:44:27 +0000

    Have Net-SNMP load all vendored MIBs in test runs
    This ensures all MIB lookups work properly when running the test suite.

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,21 @@ from zino.time import now
 from zino.trapd import TrapReceiver
 
 
+def pytest_configure(config):
+    import os
+
+    from netsnmpy import netsnmp
+
+    # Ensure that the vendored MIBs are loaded
+    os.environ["MIBS"] = "ALL"
+    vendored_mibs = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "mibs"))
+    print(f"Setting MIBDIRS to {vendored_mibs}")
+    os.environ["MIBDIRS"] = f"{vendored_mibs}"
+    netsnmp.load_mibs()
+    modules = ", ".join(sorted(netsnmp.get_loaded_mibs()))
+    print(f"Loaded MIB modules: {modules}")
+
+
 @pytest.fixture
 def polldevs_conf(tmp_path):
     name = tmp_path.joinpath("polldevs.cf")
