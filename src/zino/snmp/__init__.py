e06077d9379db5240b961565c9c8bf43360e14ef
Morten Brekkevold <morten.brekkevold@sikt.no>
2025-01-22 11:34:04 +0000

    Switch to new netsnmpy-based SNMP backend

--- src/zino/snmp/__init__.py
+++ src/zino/snmp/__init__.py
@@ -1,4 +1,5 @@
 """Zino SNMP back-ends"""
 
 # There is only one at the moment:
-from .pysnmp_backend import *  # noqa
+# from .pysnmp_backend import *  # noqa
+from .netsnmpy_backend import *  # noqa
