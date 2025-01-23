a24f009754141b0269f4e52c6acf37722a9b66ad
Morten Brekkevold <morten.brekkevold@sikt.no>
2025-01-23 13:03:55 +0000

    Keep global registry of SNMP session objects
    Before this, every Task instance would create its own SNMP session
    instance.  This might not mean much under PySNMP, but is wasteful
    using netsnmp-cffi, as each open session takes up a socket resource.
    Having multiple simultaneous sessions/sockets per poll device makes
    no sense, so `get_snmp_session()` utilizes a `WeakValueDictionary`
    to keep track of the existing session for each device and returns
    an existing session if there is one.

--- src/zino/snmp/__init__.py
+++ src/zino/snmp/__init__.py
@@ -1,5 +1,27 @@
 """Zino SNMP back-ends"""
 
-# There is only one at the moment:
+from weakref import WeakValueDictionary
+
+from zino.config.models import PollDevice
+
+# Select one of the two SNMP back-ends:
 # from .pysnmp_backend import *  # noqa
 from .netsnmpy_backend import *  # noqa
+from .netsnmpy_backend import SNMP
+
+_snmp_sessions = WeakValueDictionary()
+
+
+def get_snmp_session(device: PollDevice) -> SNMP:
+    """Create or re-use an existing SNMP session for a device.
+
+    This keeps a registry of existing/re-usable SNMP sessions so we avoid duplicate instances and over-use of sockets
+    """
+    if _snmp_sessions is None:  # Tests suite may disable session re-use
+        return SNMP(device)
+    # We generate a session key based on every attribute that affects how the SNMP session is set up:
+    key = (device.address, device.community, device.hcounters)
+    session = _snmp_sessions.get(key)
+    if not session:
+        session = _snmp_sessions[key] = SNMP(device)
+    return session
