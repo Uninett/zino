294e2763c4ade0b2e84ea6ce8c54a1fa0e162dde
Johanna England <johanna.england@sikt.no>
2025-09-23 14:48:28 +0200

    Create reachability reachable event for new device
    Only if config options is set to True
    
    The config object cannot be imported into this module at import time,
    as that would cause an empty config object to be imported.  Instead,
    the state module is imported, and the config value is copied at runtime
    into each instance of the reachabletask.

--- src/zino/tasks/reachabletask.py
+++ src/zino/tasks/reachabletask.py
@@ -2,6 +2,7 @@ import logging
 
 from apscheduler.jobstores.base import JobLookupError
 
+import zino.state
 from zino.scheduler import get_scheduler
 from zino.statemodels import ReachabilityEvent, ReachabilityState
 from zino.tasks.errors import DeviceUnreachableError
@@ -16,6 +17,7 @@ class ReachableTask(Task):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._scheduler = get_scheduler()
+        self._make_events_for_new_devices = zino.state.config.event.make_events_for_new_devices
 
     async def run(self):
         """Checks if device is reachable. Schedules extra reachability checks if not."""
@@ -27,17 +29,22 @@ class ReachableTask(Task):
             event = self.state.events.get_or_create_event(self.device.name, None, ReachabilityEvent)
             if event.reachability != ReachabilityState.NORESPONSE:
                 event.reachability = ReachabilityState.NORESPONSE
-                event.add_log(f"{self.device.name} no-response")
+                if self.device_state.reachable_in_last_run is None and self._make_events_for_new_devices:
+                    event.add_log(f"New device: {self.device.name} no-response")
+                else:
+                    event.add_log(f"{self.device.name} no-response")
             event.polladdr = self.device.address
             event.priority = self.device.priority
             self.state.events.commit(event)
-            self.state.devices[self.device.name].reachable_in_last_run = False
+            self.device_state.reachable_in_last_run = False
             self._schedule_extra_job()
             raise DeviceUnreachableError
         else:
             _logger.debug("Device %s is reachable", self.device.name)
             self._update_reachability_event_as_reachable()
-            self.state.devices[self.device.name].reachable_in_last_run = True
+            if self.device_state.reachable_in_last_run is None and self._make_events_for_new_devices:
+                self._post_reachability_reachable_event_for_new_device()
+            self.device_state.reachable_in_last_run = True
 
     async def _run_extra_job(self):
         try:
@@ -85,3 +92,9 @@ class ReachableTask(Task):
 
     def _get_extra_job_name(self):
         return f"reachabletask_{self.device.name}"
+
+    def _post_reachability_reachable_event_for_new_device(self):
+        event = self.state.events.create_event(self.device.name, None, ReachabilityEvent)
+        event.add_log(f"New device: {self.device.name} reachable")
+        event.reachability = ReachabilityState.REACHABLE
+        self.state.events.commit(event)
